553035bda086ade9b7f29b50ba20be2d4c0f0c76
Sitoh7 <ha.wakhule@gmail.com>
2024-09-30 13:49:22 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution here
+
+function findMatching(array,name){
+    const filteredArray = array.filter(function(num){
+       return num.toLowerCase() == name.toLowerCase()
+        
+    })
+   return filteredArray
+    
+}
+
+function fuzzyMatch(array,string){
+    const filteredArray = array.filter(function(num){
+        return num.substring(0,string.length) == string.substring(0, string.length)
+    })
+    return filteredArray
+}
+
+function matchName(array, name){
+    const filteredArray = array.filter(function(num){
+        return num.name == name
+    })
+   return filteredArray
+}
